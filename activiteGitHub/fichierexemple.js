c33001a1604b8f0a4f9ed4222880b6a3cd73b2e3
unknown <manuel@antunes.me>
2019-01-30 11:08:15 +0100

    ajotu d'un fichier javascript qui fait une simulation de tirage de dé

---
+++ activiteGitHub/fichierexemple.js
@@ -0,0 +1,17 @@
+function lancerDes(n, callback) {
+// génère 8 lancers de dés
+	// script honteusement pompé sur le net /!\
+if(n!=8)
+{
+// génère un random entre 1 et 6
+var de1=Math.floor(Math.random()*6)+1;
+//Affichage du score du dé dans la cellule menu
+document.getElementById('menu').innerHTML =de1;
+//On relance la fonction au bout de 200 ms
+setTimeout(function(){lancerDes(n+1, callback)}, 200);
+} else {
+callback();
+}
+}
+ 
+lancerDes(1, function(){alert("8 lancés de dé ont été faits")});
