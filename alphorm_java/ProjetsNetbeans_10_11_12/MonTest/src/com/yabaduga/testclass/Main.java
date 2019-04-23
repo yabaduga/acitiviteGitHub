099047a75157168b2d451118888ebef197bbb527
ManuelAntunes <manuel@antunes.me>
2019-04-23 19:55:56 +0200

    montestLivre pour création objet

--- alphorm_java/ProjetsNetbeans_10_11_12/MonTest/src/com/yabaduga/testclass/Main.java
+++ alphorm_java/ProjetsNetbeans_10_11_12/MonTest/src/com/yabaduga/testclass/Main.java
@@ -1,8 +1,26 @@
 package com.yabaduga.testclass;
 
+import java.io.FileWriter;
+import java.util.Scanner;
+
 public class Main {
-    public static void main (String[] args){
-    Livre monLivre=new Livre("Letitre","l'auteur",2015);
-    System.out.println(monLivre.getTitre() +" "+ monLivre.getAuteur()+" "+monLivre.getAnnee());
-}
+    public static void main (String[] args) {
+        System.out.println("Saisissez le titre, puis l'auteur en enfin l'année, le tout séparé par un retour charriot");
+        Scanner sc = new Scanner(System.in);
+        String monTitre = sc.nextLine();
+        String monAuteur = sc.nextLine();
+        int monAnnee = sc.nextInt();
+
+        Livre monLivre = new Livre(monTitre, monAuteur, monAnnee);
+        try {
+            FileWriter fw = new FileWriter("e:\\testlivre.txt");
+
+            fw.write(monLivre.getTitre() + " écrit par " + monLivre.getAuteur() + " en " + monLivre.getAnnee()+ System.getProperty( "line.separator" ));
+            fw.write(System.getProperty( "line.separator" ));
+            fw.close();
+            System.out.println(monLivre.getTitre() + " écrit par " + monLivre.getAuteur() + " en " + monLivre.getAnnee());
+
+        }catch(Exception e){System.out.println(e);}
+        System.out.println("Success...");
+    }
 }
