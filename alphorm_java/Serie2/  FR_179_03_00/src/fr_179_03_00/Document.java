4755728b57c384b19de2258c8684642b5791fe7c
ManuelAntunes <manuel@antunes.me>
2019-04-27 17:39:17 +0200

    classe et visibilité

---
+++ alphorm_java/Serie2/  FR_179_03_00/src/fr_179_03_00/Document.java
@@ -0,0 +1,17 @@
+package fr_179_03_00;
+
+public final class Document {
+
+
+    public String setTitreDuDocument;
+    private String titreDuDocument = "inconnu";
+
+   // void  setTitreDuDocument(String t) {
+       //  titreDuDocument=t;
+
+
+   public void setTitreDuDocument(String t) {
+        titreDuDocument = t;
+
+    }
+}
---
+++ alphorm_java/Serie2/  FR_179_03_00/src/fr_179_03_00/FR_179_03_00.java
@@ -0,0 +1,12 @@
+package fr_179_03_00;
+
+public class FR_179_03_00 {
+
+    public static void main (String [] agrs) {
+
+        Document doc = new Document();
+        doc.setTitreDuDocument = "La conscience même de soi";
+    }
+
+    }
+
