6b76bc89668f6262c4b85cee373dce1baaafb450
ManuelAntunes <manuel@antunes.me>
2019-05-01 13:53:28 +0200

    modification faite au mastermind reste le souci que la combinaison est prise gagnante même dans le désordre

--- rechercheandmind/src/rechercheandmind/CompareSecProp.java
+++ rechercheandmind/src/rechercheandmind/CompareSecProp.java
@@ -6,85 +6,101 @@ public class CompareSecProp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int es;
+		for (es = 1; es < 11; es++) {
+			int a = 1, b = 3, c = 2, d = 5;
+			int A, B, C, D;
+			int score=0;
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Saisissez la combinaison A puis entrée, B puis entrée,... : ");
+			A = sc.nextInt();
+			B = sc.nextInt();
+			C = sc.nextInt();
+			D = sc.nextInt();
 
-		int a =1, b=3,c=2,d=5;
-		int A,B,C,D;
-		Scanner sc=new Scanner(System.in);
-		 System.out.println("Saisissez la combinaison A puis entrée, B puis entrée,... : ");
-		    A = sc.nextInt(); 
-		    B = sc.nextInt();
-		    C = sc.nextInt();
-		    D = sc.nextInt();
-		    
-		
-		int CombSec[] = {a,b,c,d};
-		int Proposition[]= {A,B,C,D};
-		for (int i = 0; i < Proposition.length; i++) {
-		    boolean present = false;
-		    for (int j = 0; j < CombSec.length; j++) {
-		        if (CombSec[i]==A) {
-				present = true;
-				break;
+
+			int CombSec[] = {a, b, c, d};
+			int Proposition[] = {A, B, C, D};
+			for (int i = 0; i < Proposition.length; i++) {
+				boolean present = false;
+				for (int j = 0; j < CombSec.length; j++) {
+					if (CombSec[i] == A) {
+						present = true;
+						score =1;
+						break;
+					}
+				}
+				if (present) {
+					System.out.print(" oui ");
+				} else {
+					System.out.print(" non ");
+
+				}
+			}// permet un saut de ligne entre les A, B ...
+
+			System.out.println("");
+			for (int i = 0; i < Proposition.length; i++) {
+				boolean present = false;
+				for (int j = 0; j < CombSec.length; j++) {
+					if (CombSec[i] == B) {
+						present = true;
+						score = score+1;
+						break;
+					}
+				}
+				if (present) {
+					System.out.print(" oui ");
+				} else {
+					System.out.print(" non ");
+
+				}
+
 			}
-		    }
-		    if (present){
-		        System.out.print("A oui ");
-		    } else {
-			System.out.print("A non ");
-			
-		    }
-		}// permet un saut de ligne entre les A, B ...
-		
-		System.out.println("");
-		for (int i = 0; i < Proposition.length; i++) {
-		    boolean present = false;
-		    for (int j = 0; j < CombSec.length; j++) {
-		        if (CombSec[i]==B) {
-				present = true;
-				break;
+			System.out.println("");
+			for (int i = 0; i < Proposition.length; i++) {
+				boolean present = false;
+				for (int j = 0; j < CombSec.length; j++) {
+					if (CombSec[i] == C) {
+						present = true;
+						score=score+1;
+						break;
+					}
+				}
+				if (present) {
+					System.out.print(" oui ");
+				} else {
+					System.out.print(" non ");
+
+				}
+
 			}
-		    }
-		    if (present){
-		        System.out.print("B oui ");
-		    } else {
-			System.out.print("B non ");
-			
-		    }
+			System.out.println("");
 
-	}System.out.println("");
-		for (int i = 0; i < Proposition.length; i++) {
-		    boolean present = false;
-		    for (int j = 0; j < CombSec.length; j++) {
-		        if (CombSec[i]==C) {
-				present = true;
-				break;
+			for (int i = 0; i < Proposition.length; i++) {
+				boolean present = false;
+				for (int j = 0; j < CombSec.length; j++) {
+					if (CombSec[i] == D) {
+						present = true;
+						score=score+1;
+						es=11;
+
+							break;
+					}
+				}
+				if (present) {
+					System.out.println(" oui ");
+					if (score ==4) {
+						System.out.println("vous avez gagné");
+						break;
+					}
+				} else {
+					System.out.print(" non ");
+
+				}
+
 			}
-		    }
-		    if (present){
-		        System.out.print("C oui ");
-		    } else {
-			System.out.print("C non ");
-			
-		    }
-
-}System.out.println("");
-
-	for (int i = 0; i < Proposition.length; i++) {
-	    boolean present = false;
-	    for (int j = 0; j < CombSec.length; j++) {
-	        if (CombSec[i]==D) {
-			present = true;
-			break;
+			System.out.println("");
 		}
-	    }
-	    if (present){
-	        System.out.print("D oui ");
-	    } else {
-		System.out.print("D non ");
-		
-	    }
-	    
-}System.out.println("");
 	}
-	}
+}
 
