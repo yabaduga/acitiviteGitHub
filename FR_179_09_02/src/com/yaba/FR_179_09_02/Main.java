cd02bcdda585ea679ea4fb5269a82f9d24b85f14
ManuelAntunes <manuel@antunes.me>
2019-05-09 00:42:43 +0200

    utilisation de StringTokenizer manipulation et analyse d'un texte mot groupe de mot (délimiteur) comptage d'une valeur (d'un caractère)

--- FR_179_09_02/src/com/yaba/FR_179_09_02/Main.java
+++ FR_179_09_02/src/com/yaba/FR_179_09_02/Main.java
@@ -1,6 +1,6 @@
 package com.yaba.FR_179_09_02;
 
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
 
@@ -15,6 +15,27 @@ public class Main {
             System.out.println(mot);
 
 
+        }
+        char[] lesCaracteres = leTexte.toLowerCase().toCharArray();
+        // on récupère la chaine sous forme de caractères unitaires
+        // on les passes en minuscules pour qu'il n'y ait pas de différence dnas le comptage entre les casses
+        // on passe ces caractères dans un tableau lesCaracteres
+
+        Map<Character,Integer>lettres=new HashMap <>();
+        // la map permet de gérer la clé et le caractère
+        // clé est le caractère et l'integer est le nb de fois ou il est rencontré
+        for (char c: lesCaracteres) {
+            Integer v = lettres.get(c);
+            if(v==null) {
+                v=1; // bowing auto -> si n' jamais été rencontré, on le positionne à 1 (puisque premier comptage
+            }else{
+                v++; // si déjà rencontré la valeur prend +1
+            }
+            lettres.put(c,v);
+        }
+        for (char c : lettres.keySet()){
+            // pour chaque clé, on a le compte que l'on remonte
+            System.out.println("Char " +c+" -> "+lettres.get(c));
         }
     }
 }
