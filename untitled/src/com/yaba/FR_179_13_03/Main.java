3fab231db768aa6e56a5d754bb4caa937d4c0eca
ManuelAntunes <manuel@antunes.me>
2019-05-13 23:15:56 +0200

    pb de modification d'un objet par plusieurs threads un objet non mutable = va éviter le souci puisque ne sera jamais modifier = des recréations seront faites

---
+++ untitled/src/com/yaba/FR_179_13_03/Main.java
@@ -0,0 +1,35 @@
+package com.yaba.FR_179_13_03;
+
+public class Main {
+   static Personne p = new Personne("x", "x");
+    public static void main(String[] args) throws Exception {
+int count=0;
+
+        for (; ; ) {
+           count++;
+            Thread t1 = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    p=p.nomme("Manuel", "Antunes");
+                    System.out.println(p);
+                }
+            });
+            Thread t2 = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    p=p.nomme("Bob", "l'Ours");
+                    System.out.println(p);
+                }
+            });
+            t1.start();
+            t2.start();
+
+            t1.join();
+            t2.join();
+
+            String nom = p.getNom();
+            String prenom = p.getPrenom();
+            assert prenom.equals("Manuel") && nom.equals("Antunes") || prenom.equals("Bob") && nom.equals("l'Ours") || prenom.equals("x") && nom.equals("x") : "Erreur " + prenom + " ," + nom +", "+ count;
+        }
+    }
+}
---
+++ untitled/src/com/yaba/FR_179_13_03/Personne.java
@@ -0,0 +1,23 @@
+package com.yaba.FR_179_13_03;
+
+public class Personne {
+    private  String prenom;
+    private  String nom;
+
+     Personne(String p, String nom) {
+         this.prenom=p;
+         this.nom=nom;
+    }
+
+     Personne nomme(String prenom, String nom) {
+         return new Personne(prenom, nom);
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+}
